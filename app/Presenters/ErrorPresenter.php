009de0cd2c119dcb3dcc4ddd961be7559c2a7af4
danielhrenak <daniel.hrenak@gmail.com>
2024-04-21 21:03:49 +0200

    Error handling simplfied
    just one error presenter and one template

---
+++ app/Presenters/ErrorPresenter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+/**
+ * Handles 4xx HTTP error responses.
+ */
+final class ErrorPresenter extends Nette\Application\UI\Presenter
+{
+	// allow access via all HTTP methods
+	public array $allowedMethods = [];
+
+
+	public function renderDefault(Nette\Application\BadRequestException $exception): void
+	{
+		$this->template->httpCode = $exception->getCode();
+		$this->template->exceptionMessage = $exception->getMessage();
+		$this->template->setFile( __DIR__ . '/templates/Error/default.latte');
+	}
+}
